4f32d89bcccd5c5abefa75bafafc1002415fc974
Matthew Gilman <gilmam@rpi.edu>
2016-04-25 23:11:54 -0400

    test 2

--- src/app/settings.js
+++ src/app/settings.js
@@ -32,7 +32,7 @@ Settings.providers = {
         order: 1,
         name: 'Movies',
         uri: ['vodo', 'archive',
-          //'stremio?auth={"url":"http://api8.herokuapp.com","key":"423f59935153f2f5d2db0f6c9b812592b61b3737"}&url=http://localhost:9005'
+            //'stremio?auth={"url":"http://api8.herokuapp.com","key":"423f59935153f2f5d2db0f6c9b812592b61b3737"}&url=http://localhost:9005'
         ]
     },
     tvshow: {
@@ -327,10 +327,10 @@ var AdvSettings = {
                     tryNextEndpoint();
                 });
             }).setTimeout(5000, function () {
-				win.warn('[%s] Endpoint timed out',
-					_url.hostname);
-				request.abort();
-				tryNextEndpoint();
+                win.warn('[%s] Endpoint timed out',
+                    _url.hostname);
+                request.abort();
+                tryNextEndpoint();
             });
         } else {
             tls.connect(443, _url.hostname, {
