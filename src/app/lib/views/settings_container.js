346e733ca23b0f7a0cd73d31ff9ec2db7fcdc898
Matthew Gilman <gilmam@rpi.edu>
2016-04-28 22:57:13 -0400

    Reverse

--- src/app/lib/views/settings_container.js
+++ src/app/lib/views/settings_container.js
@@ -710,9 +710,7 @@
 
             App.Trakt.syncTrakt.all()
                 .then(function () {
-                    App.Providers.get('Watchlist').fetch({
-                        force: true
-                    });
+                    App.Providers.get('Watchlist').fetch({force: true});
                 })
                 .then(function () {
                     $('#syncTrakt')
